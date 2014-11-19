fbb29311c0266db26bb9b2d50f680a852a540420
suzp1984 <suzp1984@gmail.com>
2014-11-19 22:19:49 +0800

    refactor Volley: newRequestQueueWithCustomNetwork/WithCustomHttpStack

--- src/com/android/volley/toolbox/Volley.java
+++ src/com/android/volley/toolbox/Volley.java
@@ -33,13 +33,52 @@ public class Volley {
     private static final String DEFAULT_CACHE_DIR = "volley";
 
     /**
+     * Create a default instance of the worker pool and calls {@link RequestQueue#start()} on it.
+     * 
+     * @param context
+     * @param stack
+     * @param network
+     * @return
+     */
+    public static RequestQueue newRequestQueueWithCustomNetwork(Context context, Network network) {
+        File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
+
+        String userAgent = "volley/0";
+        try {
+            String packageName = context.getPackageName();
+            PackageInfo info = context.getPackageManager().getPackageInfo(packageName, 0);
+            userAgent = packageName + "/" + info.versionCode;
+        } catch (NameNotFoundException e) {
+        }
+
+
+        if (network == null) {
+            HttpStack stack = null;
+            if (Build.VERSION.SDK_INT >= 9) {
+                stack = new HurlStack();
+            } else {
+                // Prior to Gingerbread, HttpUrlConnection was unreliable.
+                // See: http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+                stack = new HttpClientStack(AndroidHttpClient.newInstance(userAgent));
+            }
+
+            network = new BasicNetwork(stack);
+        }
+
+        RequestQueue queue = new RequestQueue(new DiskBasedCache(cacheDir), network);
+        queue.start();
+
+        return queue;
+    }
+
+    /**
      * Creates a default instance of the worker pool and calls {@link RequestQueue#start()} on it.
      *
      * @param context A {@link Context} to use for creating the cache dir.
      * @param stack An {@link HttpStack} to use for the network, or null for default.
      * @return A started {@link RequestQueue} instance.
      */
-    public static RequestQueue newRequestQueue(Context context, HttpStack stack) {
+    public static RequestQueue newRequestQueueWithCustomHttpStack(Context context, HttpStack stack) {
         File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
 
         String userAgent = "volley/0";
@@ -75,6 +114,6 @@ public class Volley {
      * @return A started {@link RequestQueue} instance.
      */
     public static RequestQueue newRequestQueue(Context context) {
-        return newRequestQueue(context, null);
+        return newRequestQueueWithCustomHttpStack(context, null);
     }
 }
